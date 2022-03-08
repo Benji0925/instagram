374d6a8f1c7c265b8400c3b3903bb455eed67965
Binyam Assefa <binia6181@gmail.com>
2022-03-08 16:21:11 -0600

    added sign in and sign up

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "6nZICwJpPdALncDGQ5tvi6c0edWTfQuN6PlrexuF"
+            $0.clientKey = "oUFzFFPP3g94yjjA6UN1R7nySwHjacgxWTNQZgMe"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: configuration)
         return true
     }
 
